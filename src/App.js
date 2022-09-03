f2e903d845d341559048433e79dbce7414c9fcc2
Leonel Gallardo <leonelgallardo87@gmail.com>
2022-09-02 23:46:17 -0300

    App Carritos de Compra - FEIII

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+import {useState} from "react";
+
+
+
+
+function App() {
+
+const [cantidadProductos, setCantidadProductos] = useState(0);
+
+function aumentarCantidad(){
+  const cantidad = cantidadProductos + 1
+  setCantidadProductos(cantidad);
+
+};
+
+  return (
+    <div className="App">
+      <Cabecera totalProductos = {cantidadProductos}/>
+      <Listado aumentar = {aumentarCantidad}/>
+    </div>
+  );
+}
+
+export default App;
